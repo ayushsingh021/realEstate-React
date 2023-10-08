93277322a5bf5da489767016a8cd89e973cb3bb1
unknown <iamayushsingh03@gmail.com>
2023-10-08 17:15:41 +0530

    added the sidenavbar and add routing/navigation

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Offers from "./pages/Offers";
 import ForgotPassword from "./pages/ForgotPassword";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Router>
+        <Header/>
         <Routes>
           <Route path="/" element = {<Home/>} />
           <Route path="/profile" element = {<Profile/>} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import {MdHome,MdPercent, MdPermIdentity} from "react-icons/md";
+import { ReactComponent as Logo } from '../assets/logobrand.svg';
+import { useLocation, useNavigate } from 'react-router-dom';
+export default function Header() {
+  
+  
+  return (
+   
+    <div className='fixed top-0 left-0 h-screen w-16 m-0
+                    flex flex-col
+                    bg-gray-900 text-white shadow-lg'> 
+                  
+           
+             
+      {/* Logo is an actual React component */}
+      <Logo className='sidebar-icon' />
+    
+        <HeaderIcon way={"/"} icon={<MdHome size="28" />} text='Home'/>
+        <HeaderIcon way={"/offers"} icon={<MdPercent size="28" />}text='Offers'/>
+        <HeaderIcon way={"/sign-in"} icon={<MdPermIdentity size="28"/>} text='SignIn'/>
+    
+    </div>
+  )
+};
+
+function HeaderIcon({ icon , text = 'tooltip' ,way}) {
+    const location = useLocation(); //returns the path of the object -- .pathname is imp part
+    const navigate = useNavigate() ; //helps to do navigation in react routes
+
+    function pathMathRoute(route){ //will change css if clicked or not
+        if(route === location.pathname){
+            return true;
+        }
+    }
+
+    return (
+        <div onClick={()=>navigate(way)} className = {`sidebar-icon group ${pathMathRoute(way) && "bg-green-600 text-white"}`}>
+            {icon}
+            <span class = "sidebar-tooltip group-hover:scale-100">
+                {text}
+            </span>
+        </div>
+
+    );
+}
