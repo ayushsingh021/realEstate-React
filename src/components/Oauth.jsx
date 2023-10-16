2e2489a41efae80b613b881fe8f2f01362d9da89
Ayush021 <12114021@nitkkr.ac.in>
2023-10-16 09:42:49 +0530

    completed google oauth functionality

--- src/components/Oauth.jsx
+++ src/components/Oauth.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import {FcGoogle} from 'react-icons/fc'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { toast } from 'react-toastify';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
+import { db } from '../firebase';
+import { useNavigate } from 'react-router-dom';
+
 export default function Oauth() {
+  const navigate = useNavigate();
+  async function onGoogleClick(){
+    try {
+      
+      const auth = getAuth();
+      const provider  = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth , provider);
+      const user  = result.user;
+      console.log(user);
+      //cheak if user already existed or not
+      const docRef = doc(db , "users" , user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if(!docSnap.exists()){
+        await setDoc(docRef , {
+          name : user.displayName,
+          email : user.email,
+          timestamp : serverTimestamp()
+        })
+      }
+      //home page navigation after authencticaitn
+      navigate("/")
+
+    } 
+    catch (error) {
+      toast.error('Google authentication failed!');
+      console.log(error);
+    }
+  }
   return (
+    
+
     <div>
-      <button className='flex items-center 
+      <button type="button"
+      onClick={onGoogleClick} className='flex items-center 
       justify-center w-full
       bg-red-700 text-white
       px-7 py-3 uppercase
