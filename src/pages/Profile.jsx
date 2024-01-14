6f203ab4b274cca7ca24a723656e9f34899faf31
Ayush021 <12114021@nitkkr.ac.in>
2024-01-14 14:10:03 +0530

    added edit details functionality to the profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,10 @@
 import { onLog } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, updateProfile } from 'firebase/auth';
+import { doc, updateDoc } from 'firebase/firestore';
 import {useState , React} from 'react'
 import { useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
+import { db } from '../firebase';
 
 export default function Profile() {
   const auth = getAuth();
@@ -10,6 +13,36 @@ export default function Profile() {
     name : auth.currentUser.displayName,
     email :auth.currentUser.email,
   });
+  const [chageDatails , setChangeDetails] = useState(false);
+
+  async function onSubmit(){
+    try {
+      if(auth.currentUser.displayName !== name){
+        //update displayname in the firebase auth it is changed here 
+        await updateProfile(auth.currentUser,{
+          displayName:name,
+        });
+        
+        //update name in the firestore
+        const docRef = doc(db , "users" , auth.currentUser.uid)
+        await updateDoc(docRef,{
+          name : name
+        })
+        toast.success('Updated Successfully')
+
+      }
+      
+    } catch (error) {
+      toast.error('Could Not Change')
+    }
+  }
+  function onChange(e){
+      setFormData((prevState) =>({
+        ...prevState,
+        [e.target.id] : e.target.value,
+      }))
+  }
+
   function onLogOut(){
     auth.signOut();
     navigate("/");
@@ -28,9 +61,12 @@ export default function Profile() {
         <form >
           {/* name input/\ */}
           <input type="text " id='name' value={name} 
-          disabled className='mb-6 w-full px-4 py-2 test-xl
+          disabled = {!chageDatails}
+          onChange={onChange}
+          className={`mb-6 w-full px-4 py-2 test-xl
           text-gray-700 bg-white border border-gray-300
-          rounded transition ease-in-out'/>
+          rounded transition ease-in-out ${chageDatails && "bg-blue-200 focus: bg-blue-200"}`}
+          />
 
            {/* email input/\ */}
            <input type="email " id='email' value={email} 
@@ -43,7 +79,14 @@ export default function Profile() {
               Do you want to change your name?
               <span className='text-red-600 hover:text-red-700
               transition ease-in-out
-              duration-200 ml-1 cursor-pointer'>Edit</span>
+              duration-200 ml-1 cursor-pointer'
+              onClick={()=>{
+                chageDatails && onSubmit()
+                setChangeDetails((prev) => !prev)
+              }}
+              >
+                {chageDatails ? "Apply" : "Edit"}
+              </span>
             </p>
             <p onClick={onLogOut} className='text-blue-600 hover:text-blue-800
             transition duration-200 ease-in-out cursor-pointer'>
