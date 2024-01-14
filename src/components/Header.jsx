4dc90934b76c77df6afdc110d91ad96e5762d49d
Ayush021 <12114021@nitkkr.ac.in>
2024-01-14 16:00:34 +0530

    completed the sidebar making profile and signin text dynamic according to user signed or not

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {MdHome,MdPercent, MdPermIdentity} from "react-icons/md";
 import { ReactComponent as Logo } from '../assets/logobrand.svg';
 import { useLocation, useNavigate } from 'react-router-dom';
+import {getAuth,onAuthStateChanged} from "firebase/auth";
 export default function Header() {
+    const [pageState ,setPageSate] = useState("Sign-In");
+    const auth = getAuth();
+    useEffect(() =>{
+        onAuthStateChanged(auth,(user)=>{
+            if(user){
+                setPageSate("Profile");
+            }else{
+                setPageSate("Sign-In");
+            }
+        })
+    },[auth])
   
   
   return (
@@ -17,26 +29,26 @@ export default function Header() {
       {/* Logo is an actual React component */}
       <Logo className='sidebar-icon' />
     
-        <HeaderIcon way={"/"} icon={<MdHome size="28" />} text='Home'/>
-        <HeaderIcon way={"/offers"} icon={<MdPercent size="28" />}text='Offers'/>
-        <HeaderIcon way={"/sign-in"} icon={<MdPermIdentity size="28"/>} text='SignIn'/>
+        <HeaderIcon pathWay = {"/"} way={"/"} icon={<MdHome size="28" />} text='Home'/>
+        <HeaderIcon pathWay={"/offers"}  way={"/offers"} icon={<MdPercent size="28" />}text='Offers'/>
+        <HeaderIcon pathWay={"/profile"} way={"/sign-in" || "/profile"} icon={<MdPermIdentity size="28"/>} text={pageState}/>
     
     </div>
   )
 };
 
-function HeaderIcon({ icon , text = 'tooltip' ,way}) {
+function HeaderIcon({ icon , text = 'tooltip' ,way ,pathWay}) {
     const location = useLocation(); //returns the path of the object -- .pathname is imp part
     const navigate = useNavigate() ; //helps to do navigation in react routes
 
-    function pathMathRoute(route){ //will change css if clicked or not
+    function pathMatchRoute(route){ //will change css if clicked or not
         if(route === location.pathname){
             return true;
         }
     }
 
     return (
-        <div onClick={()=>navigate(way)} className = {`sidebar-icon group ${pathMathRoute(way) && "bg-green-600 text-white"}`}>
+        <div onClick={()=>navigate(pathWay)} className = {`sidebar-icon group ${pathMatchRoute(way) && "bg-green-600 text-white"}`}>
             {icon}
             <span class = "sidebar-tooltip group-hover:scale-100">
                 {text}
