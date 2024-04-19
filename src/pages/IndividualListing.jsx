7fb3796ebab4de67e0f82107afd174e166e8b6fe
Ayush021 <12114021@nitkkr.ac.in>
2024-04-20 02:32:04 +0530

    added share feature for indiv list

--- src/pages/IndividualListing.jsx
+++ src/pages/IndividualListing.jsx
@@ -7,6 +7,7 @@ import { doc, getDoc } from "firebase/firestore";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from 'swiper/modules';
+import { FaShare } from "react-icons/fa";
 
 // Import Swiper styles
 import 'swiper/css';
@@ -20,6 +21,7 @@ function IndividualListing() {
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
 
  
 
@@ -47,6 +49,7 @@ function IndividualListing() {
       spaceBetween={15}
       pagination={{
         clickable: true,
+        
       }}
       modules={[Pagination]}
       className="mySwiper"
@@ -63,6 +66,23 @@ function IndividualListing() {
         </SwiperSlide>
       ))}
     </Swiper>
+    <div
+        className="fixed top-[2%] right-[1%] z-10 bg-green-600 cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <FaShare className="text-lg text-white" />
+      </div>
+      {shareLinkCopied && (
+        <p className="fixed top-[6%] right-[4%] font-semibold border-1 border-gray-400 rounded-md bg-white z-10 p-2">
+          Link Copied!
+        </p>
+      )}
   </main>
   )
  
