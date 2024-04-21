637ba67d6fe88d8d9cd8f8be0c5ddd95e908ca1c
Ayush021 <12114021@nitkkr.ac.in>
2024-04-21 14:04:30 +0530

    created complete home page and added places to sell , places to rent and offers created indexes in firestore console

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -28,7 +28,7 @@ export default function ListingItem({ listing, id ,  onEdit, onDelete }) {
           </div>
           <p className="font-semibold m-0 text-xl truncate">{listing.name}</p>
           <p className="text-[#457b9d] mt-2 font-semibold">
-            $
+          ₹
             {listing.offer
               ? listing.discountedPrice
                   .toString()
